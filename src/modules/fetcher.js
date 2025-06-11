d5b4efe690e0e8e49b28484dc9a0443777f1bbc9
eustaquio <eustaquio.galano@gmail.com>
2025-06-11 18:42:05 +0800

    feat: add function to fetch data from weather API

--- src/modules/fetcher.js
+++ src/modules/fetcher.js
@@ -1,12 +1,12 @@
-export async function fetchWeatherData() {
+export async function fetchWeatherData(location) {
   try {
-    const location = prompt('Enter a city: ', 'manila');
+    // const location = prompt('Enter a city: ', 'manila');
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=7KG7V6LV3EKE4JDUQPBZ8EBLX&contentType=json`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=7KG7V6LV3EKE4JDUQPBZ8EBLX&contentType=json`
     );
     const locationData = await response.json();
     return locationData;
   } catch (error) {
-    throw new Error(error);
+    console.log(new Error(error));
   }
 }
